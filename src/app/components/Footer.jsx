2c99a7c5df48719cdcfed9f89757e8aa5365ff85
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2023-11-23 21:34:57 +0500

    update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer text-white text-1xl bg-[#545454]">
-      <div className="p-12 flex justify-between">
+    <footer className="footer border z-10border-t-[#33353F] border-l-transparent border-r-transparent text-white text-1xl bg-[#545454]">
+      <div className="container p-12 flex justify-between">
         <span>LOGO</span>
         <p className="text-zinc-300">All rights reserved.</p>
       </div>
