90f79bd037b6979b8da20169ca3225bd2a7a662d
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2023-11-27 18:09:32 +0500

    add responsive design to the FAQ Section

--- src/app/components/FAQSection.jsx
+++ src/app/components/FAQSection.jsx
@@ -4,12 +4,12 @@ const EmailSection = () => {
   return (
     <section
       id="contact"
-      className="my-12 md:my-12 py-0 relative"
+      className="my-12 lg:my-20 mx-0 lg:mx-20"
     >
-      <div className="z-auto mb-12">
-        <h2 className="text-4xl font-semibold text-zinc-700 mb-12">
+      <div className="mb-12">
+        <h2 className="text-2xl lg:text-4xl font-semibold text-zinc-700 mb-10">
           Часто задаваемые вопросы
-          <div className="w-[1430px] h-[1px] ml-0 mt-2 absolute bg-neutral-400"></div>
+          <div className="w-full lg:w-[1280px] h-[1px] ml-0 mt-3 bg-neutral-400"/>
         </h2>
                   
         <div class="u-accordion">
@@ -27,7 +27,7 @@ const EmailSection = () => {
             Да, наш сервис предоставляет возможность отслеживать статус вашей заявки в режиме реального времени. Вы сможете узнать, на каком этапе находится ваша заявка.
           </div>
         </div>
-        <div className="w-[1120px] h-[1px] ml-0 absolute bg-neutral-400"></div>
+        <div className="w-full lg:w-[1120px] h-[1px] ml-0 bg-neutral-400"/>
 
         <div class="u-accordion">
           <div class="u-accordion-title">
@@ -44,7 +44,7 @@ const EmailSection = () => {
             Да, наш сервис предоставляет возможность отслеживать статус вашей заявки в режиме реального времени. Вы сможете узнать, на каком этапе находится ваша заявка.
           </div>
         </div>
-        <div className="w-[1120px] h-[1px] ml-0 absolute bg-neutral-400"></div>
+        <div className="w-full lg:w-[1120px] h-[1px] ml-0 bg-neutral-400"/>
 
         <div class="u-accordion">
           <div class="u-accordion-title">
@@ -61,7 +61,7 @@ const EmailSection = () => {
             Да, наш сервис предоставляет возможность отслеживать статус вашей заявки в режиме реального времени. Вы сможете узнать, на каком этапе находится ваша заявка.
           </div>
         </div>
-        <div className="w-[1120px] h-[1px] ml-0 absolute bg-neutral-400"></div>
+        <div className="w-full lg:w-[1120px] h-[1px] ml-0 bg-neutral-400"/>
 
         <div class="u-accordion">
           <div class="u-accordion-title">
