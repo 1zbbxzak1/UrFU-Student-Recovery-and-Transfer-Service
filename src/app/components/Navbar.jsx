fe9d1605a85ec58ce610cf4b89f228b99461d652
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2023-11-27 14:18:08 +0500

    update navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed mx-auto top-0 left-0 right-0 z-10 bg-opacity-100">
-      <div className="w-full h-[100px] left-0 top-0 absolute bg-gradient-to-l from-blue-900 via-sky-600 to-blue-500">
+      <div className="w-full h-[100px] relative bg-gradient-to-l from-blue-900 via-sky-600 to-blue-500">
         <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
           <div className="mobile-menu block md:hidden">
             {!navbarOpen ? (
